cf6517de892f15c98f522e09b0e029f4126e44dd
cam-gibson <gbabies29@outlook.com>
2026-09-13 17:36:27 +0000

    Added the gross pay, tax withheld, and net pay.

--- paycheck.py
+++ paycheck.py
@@ -29,3 +29,14 @@
 
 employee_name = input("Enter employee name: ")
 hours_worked = float(input("Enter number of hours worked: "))
+hourly_rate = float(input("Enter your hourly rate: "))
+tax_rate = float(input("Enter tax rate, as a percent: "))
+
+gross_pay = hours_worked * hourly_rate
+tax_withheld = gross_pay * (tax_rate / 100)
+net_pay = gross_pay - tax_withheld
+
+print("Employee:", employee_name)
+print(f"Gross pay: ${gross_pay:.2f}")
+print(f"Tax withheld: ${tax_withheld:.2f}")
+print(f"Net pay: ${net_pay:.2f}")
